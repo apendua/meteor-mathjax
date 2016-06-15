27275029a95d0b6fcd6b11994a42285bcb28789d
Tomasz Lenarcik <apendua@gmail.com>
2016-06-16 00:14:45 +0200

    Update example to check if extensions work

--- example/example.js
+++ example/example.js
@@ -1,7 +1,11 @@
 if (Meteor.isClient) {
 
+  MeteorMathJax.defaultConfig.TeX = {
+    extensions: ["cancel.js"]
+  };
+
   var mathjax = new MeteorMathJax.Helper({
-    
+    useCache: true
   });
 
   Template.registerHelper('mathjax', mathjax.getTemplate());
@@ -11,14 +15,23 @@ if (Meteor.isClient) {
   var equations = [
     "$$\\frac{x^2+y^2}{z^2}=1$$",
     "$$\\sin^2x+\\cos^2x=1$$",
+    "$$\\cancel{\\sin^2x+\\cos^2x=1}$$",
   ];
 
-  Template.hello.equation = function () {
-    return equations[Session.get('index') % equations.length];
-  };
+  Template.hello.helpers({
+    equation: function () {
+      return equations[Session.get('index') % equations.length];
+    },
+  });
 
   Template.hello.events({
     'click input': function () {
+      Session.set('index', Session.get('index') + 1);
+    }
+  });
+  
+  Template.clickMe.events({
+    'click button': function () {
       Session.set('index', Session.get('index') + 1);
     }
   });
